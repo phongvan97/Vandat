743e548ede3736640c87af981704eedaa3133185
Admin <phongnv52@wru.vn>
2018-10-10 19:57:38 +0700

    dsfsdf

--- Process/AddWeek.php
+++ Process/AddWeek.php
@@ -4,10 +4,10 @@ $name=$_GET['name'];
 $sql="INSERT into week(Name) VALUES('".$name."')";
 $conn->query($sql);
 
-for ($x=1;$x<7;$x++){
+for ($x=1;$x<=7;$x++){
     $sql="INSERT INTO days(days.Week,days.DaysOfWeek) VALUES ((SELECT week.Id FROM week ORDER BY week.Id DESC LIMIT 1),".$x.")";
     $conn->query($sql);
-    for ($y=1;$y<7;$y++){
+    for ($y=1;$y<=2;$y++){
         $sql="INSERT INTO shiffofday(Shiff,Day) VALUES(".$y.",(SELECT days.Id FROM days ORDER BY days.Id DESC LIMIT 1 ))";
         $conn->query($sql);
     }
